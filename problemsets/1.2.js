c329108325215c7baf556c73d3baeb9d6691fe76
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-09 10:25:38 -0700

    finished 1.2

--- problemsets/1.2.js
+++ problemsets/1.2.js
@@ -1,6 +1,8 @@
+// #1: SUM
+
 /* imperative implementation */
 function sum(numbers) {
-  const sum = 0
+  let sum = 0
   for (let i = 0, l = numbers.length; i < l; i++) {
     sum += numbers[i]
   }
@@ -12,4 +14,30 @@ function sum2(numbers) {
   return numbers.reduce(function(sum, num) {
     return sum + num
   }, 0)
+}
+
+
+// #2: DOM MANIPULATION
+
+/* side-effects */
+function updateView() {
+  let sum = 0
+
+  document.querySelectorAll('.line-item').each(function(){
+    sum += Number(this.innerHTML)
+  })
+
+  document.querySelector('.total').text(sum)
+}
+
+/* more responsible side-effects */
+function getNumbers(collection) {
+  return collection.map(function(){
+    return Number(this.innerHTML)
+  })
+}
+
+function updateView2() {
+  document.querySelector('.total')
+    .text(sum(getNumbers(document.querySelectorAll('.line-item'))))
 }
